eb1b0ed72183355d1f492f485b411f246f68609c
prasadrajneesh86 <prasadbits86@gmail.com>
2018-08-22 16:08:37 +0530

    Added reporting section Credentials in config file

--- src/main/java/com/pwc/qa/pages/DataPage.java
+++ src/main/java/com/pwc/qa/pages/DataPage.java
@@ -1,5 +1,7 @@
 package com.pwc.qa.pages;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,29 +12,35 @@ import com.pwc.qa.base.TestBase;
 
 public class DataPage extends TestBase {
 	
-	//@FindBy(xpath="//h2[contains(text(),'Status of uploaded data ')]")
-	//WebElement dataPageLabel;
-	
-	//@FindBy(xpath="")
-	//WebElement browseButton;
-	
 	@FindBy(id="upload_data_btn")
 	WebElement uploadDataButton;
+	
+	@FindBy(xpath="(//img[@alt='delete icon'])[1]")
+	WebElement deleteButton;
+	
+	@FindBy(xpath="(//button[@type='button'])[8]")
+	WebElement closeButton;
+	
+	@FindBy(xpath="//input[@id='dropdownMenu5']")
+	WebElement browseButton;
 	
 	//Initializing the Page Objects
 	public DataPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
-	//public boolean verifyDataPageLabel() {
-		//return dataPageLabel.isDisplayed();
-	//}
-	
-	
-	public void uploadNewData(String dropdownMenu1) {
+	public void verifyValidationMessage(String dropdownMenu1) {
 		Select selectDropdown = new Select(driver.findElement(By.id("dropdownMenu1")));
 		selectDropdown.selectByVisibleText(dropdownMenu1);
 		uploadDataButton.click();
+	}
+	public void uploadFile() throws IOException, Exception {
+		
+		browseButton.click();
+		Thread.sleep(3000);
+		Runtime.getRuntime().exec("D:\\AutoTest\\Newfile.exe");
+		Thread.sleep(3000);
+		uploadDataButton.click();
+		Thread.sleep(5000);
 	}
 	
 
--- src/test/java/com/pwc/qa/testcases/AdminPageTest.java
+++ src/test/java/com/pwc/qa/testcases/AdminPageTest.java
@@ -17,8 +17,10 @@ public class AdminPageTest extends TestBase{
 	HomePage homePage;
 	TestUtil testUtil;
 	AdminPage adminPage;
-	String sheetName = "admin";
-	
+	//String sheetName = "admin";
+	//String sheetName = "business";
+	String sheetName = "sub";
+	//String sheetName = "locsheet";
 	public AdminPageTest() {
 		super();
 	}
@@ -36,19 +38,37 @@ public class AdminPageTest extends TestBase{
 		Object data[][] = TestUtil.getTestData(sheetName);
 		return data;
 	}
-	
-	
 	@Test(priority=1,dataProvider="getPWCTestData")
-	public void validateAddCompanyTest(String companyPan, String companyName) {
+	public void validateAddCompanyTest(String companyPan, String companyName) throws Exception {
 		homePage.clickOnCompanyLink();
-		//adminPage.addNewCompany("CMCLL7860M", "ClaritusConsultingPan");
+		Thread.sleep(2000);
 		adminPage.addNewCompany(companyPan, companyName);
+		Thread.sleep(2000);
 	}
-	
-	/*
+	@Test(priority=2,dataProvider="getPWCTestData")
+	public void validateAddNewBusinessUnitTest(String companyName,String businessUnit) throws InterruptedException {
+		homePage.clickOnBusinessUnitLink();
+		Thread.sleep(2000);
+		adminPage.addNewBusinessUnit(companyName,businessUnit);
+		Thread.sleep(2000);
+	}
+	@Test(priority=3,dataProvider="getPWCTestData")
+	public void validateAddNewSubBusinessUnitTest(String companyName,String businessUnit,String subBusinessUnitName) throws Exception {
+		homePage.clickOnSubBusinessUnitLink();
+		Thread.sleep(2000);
+		adminPage.addNewSubBusinessUnit(companyName, businessUnit, subBusinessUnitName);
+		Thread.sleep(2000);
+	}
+	@Test(priority=4,dataProvider="getPWCTestData")
+	public void validateAddNewLocationTest(String companyName,String addressNew1,String locatCode,String addressNew2,
+			String locationCategory,String placeNew,String locatName,String pinCod,
+			String stateNew) {
+		homePage.clickOnLocationLink();
+		adminPage.addNewLocation(companyName, addressNew1, locatCode, addressNew2, locationCategory, placeNew, locatName, pinCod, stateNew);
+	}
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 	}
-	*/
+	
 }
